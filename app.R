2d97d9e9776ac3c802653bf93c39c07f3f5cb65b
Matt Keeley <keeley55@me.com>
2024-07-01 21:05:30 -0700

    Create app.R

---
+++ app.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+# Define UI for application
+ui <- fluidPage(
+  titlePanel("Hello Shiny!"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins", 
+                  "Number of bins:", 
+                  min = 1, 
+                  max = 50, 
+                  value = 30)
+    ),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    x <- faithful$eruptions
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
